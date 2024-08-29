97e054686fc6c20e3299a4490d185cee89ee3e57
Yasuhiro Morioka <ymorioka@gmail.com>
2024-08-29 16:25:50 +0900

    add  correct/wrong operation

--- main.ts
+++ main.ts
@@ -1,25 +1,36 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (R == 0) {
-        // 最初の解答者のIDを打ち返す。解答権を与える
-        if (receivedNumber > 0) {
-            radio.sendNumber(receivedNumber)
-            R = receivedNumber
-            basic.showNumber(receivedNumber)
+    受信した子機番号 = receivedNumber % 100
+    if (解答者あり == 0) {
+        // 最初の解答者に解答権を与える
+        if (受信した子機番号 > 0) {
+            解答者あり = 1
+            子機への指示 = 100
+            radio.sendNumber(受信した子機番号 + 子機への指示)
+            basic.showNumber(受信した子機番号)
             music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
         }
     }
-    // 解答者の応答順に保持しておく
-    if (R > 0) {
-        if (receivedNumber > 0) {
-            配列.push(receivedNumber)
+    // 解答者の応答順を保持しておく
+    if (解答者あり == 1) {
+        if (受信した子機番号 > 0) {
+            配列.push(受信した子機番号)
         }
     }
 })
-// 解答者の応答順を、順次表示する。エンドでビープ
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    if (解答者あり == 1) {
+        // 解答者に「不正解」を通知する
+        子機への指示 = 300
+        radio.sendNumber(受信した子機番号 + 子機への指示)
+        basic.showIcon(IconNames.No)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     if (配列.length == 0) {
         basic.showString("M")
     } else {
+        // 解答者の応答順を、順次表示する。エンドでビープ
         basic.showNumber(配列[配列インデックス])
         配列インデックス += 1
         if (配列.length == 配列インデックス) {
@@ -28,18 +39,34 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
-// 状態をリセット
 input.onButtonPressed(Button.B, function () {
+    // 状態をリセットする
     radio.sendNumber(0)
-    R = 0
+    解答者あり = 0
     配列 = []
+    配列インデックス = 0
+    受信した子機番号 = 0
     basic.showString("M")
 })
-let R = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    // 解答者に「正解」を通知する
+    if (解答者あり == 1) {
+        子機への指示 = 200
+        radio.sendNumber(受信した子機番号 + 子機への指示)
+        basic.showIcon(IconNames.Yes)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+    }
+})
 let 配列インデックス = 0
 let 配列: number[] = []
+let 子機への指示 = 0
+let 受信した子機番号 = 0
+let 解答者あり = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
+解答者あり = 0
+受信した子機番号 = 0
+子機への指示 = 0
 配列 = []
 配列インデックス = 0
 basic.showString("M")
